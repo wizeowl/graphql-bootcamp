969f8ca448dc76f8de243e65338ace12a0016332
wizeowl <nidhal.ben.tahar@gmail.com>
2019-11-26 16:55:58 +0100

    add secret to prisma api

--- prisma/src/prisma.js
+++ prisma/src/prisma.js
@@ -2,5 +2,6 @@ import { Prisma } from 'prisma-binding';
 
 export const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'http://localhost:4466'
+  endpoint: 'http://localhost:4466',
+  secret: 'sottachsba3tach7obbimejech'
 });
