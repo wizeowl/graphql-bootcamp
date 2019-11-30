30e5223fdbb10143199d640ab1d6a2cef0831ec5
Nidhal BEN TAHAR <nidhal.ben.tahar@gmail.com>
2019-12-01 00:09:42 +0100

    test: test users query

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,13 +1,43 @@
 import 'cross-fetch/polyfill';
 import ApolloBoost, { gql } from 'apollo-boost';
 import { prisma } from '../src/prisma';
+import { validateAndHashPassword } from '../src/utils/validateAndHashPassword';
 
 const client = new ApolloBoost({
   uri: `http://localhost:${process.env.PORT}`
 });
 
+const dummyUser = {
+  name: 'Al Pacino',
+  email: 'amigo@example.com'
+};
+
 beforeEach(async () => {
+  const password = await validateAndHashPassword('azertyui');
+
+  await prisma.mutation.deleteManyPosts();
   await prisma.mutation.deleteManyUsers();
+  await prisma.mutation.createUser({
+    data: { ...dummyUser, password }
+  });
+
+  await prisma.mutation.createPost({
+    data: {
+      title: 'Post 1',
+      body: 'Body 1',
+      published: true,
+      author: { connect: { email: dummyUser.email } }
+    }
+  });
+
+  await prisma.mutation.createPost({
+    data: {
+      title: 'Post 2',
+      body: 'Body 2',
+      published: false,
+      author: { connect: { email: dummyUser.email } }
+    }
+  });
 });
 
 test('Should create a User', async () => {
@@ -51,4 +81,24 @@ test('Should create a User', async () => {
   });
 
   expect(userExists).toBeTruthy();
+});
+
+test('Should expose public author profiles', async () => {
+  const query = gql`
+    query {
+      users {
+        id
+        name
+        email
+      }
+    }
+  `;
+
+  const {
+    data: { users }
+  } = await client.query({ query });
+  expect(users).toBeTruthy();
+  expect(users.length).toEqual(1);
+  const [user] = users;
+  expect(user.email).toBeFalsy();
 });
