655e8ecb0b706159e41e1ae68e34ca08cec228c0
Nidhal BEN TAHAR <nidhal.ben.tahar@gmail.com>
2019-12-01 13:07:57 +0100

    test: test myPosts query

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,13 +1,28 @@
 import 'cross-fetch/polyfill';
 import { gql } from 'apollo-boost';
-import { getClient } from './util/getClient';
 
-import { seed } from './util/seed';
+import { getClient } from './util/getClient';
+import { seed, userOne } from './util/seed';
 
 const client = getClient();
 
 describe('Post', () => {
   beforeEach(seed);
+
+  const myPostsQuery = gql`
+    query {
+      myPosts {
+        id
+        title
+        body
+        published
+        author {
+          id
+          name
+        }
+      }
+    }
+  `;
 
   it('Should return public posts', async () => {
     const query = gql`
@@ -29,5 +44,27 @@ describe('Post', () => {
     expect(posts.length).toEqual(1);
     const [post] = posts;
     expect(post.published).toBeTruthy();
+  });
+
+  it('should fail to get my posts without auth', async () => {
+    await expect(
+      client.query({ query: myPostsQuery })
+    ).rejects.toThrow();
+  });
+
+  it('should get myPosts', async () => {
+    const authClient = getClient(userOne.jwt);
+
+    const {
+      data: { myPosts }
+    } = await authClient.query({ query: myPostsQuery });
+
+    myPosts.forEach(post => {
+      expect(post.id).toBeTruthy();
+      expect(post.title).toBeTruthy();
+      expect(post.body).toBeTruthy();
+      expect(post.published).toBeDefined();
+      expect(post.author.id).toEqual(userOne.user.id);
+    });
   });
 });
